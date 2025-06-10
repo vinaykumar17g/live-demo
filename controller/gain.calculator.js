58a7fec2a5c462f59d054d01442bf08a5bb4fd0b
vinay kumar <vk1230460@gmail.com>
2025-06-10 12:19:45 +0530

    first commit

--- controller/gain.calculator.js
+++ controller/gain.calculator.js
@@ -2,13 +2,13 @@ export let gainCalculator =async (req,res)=>
 {
     let {diameter,frequency}=req.body;
     frequency =1* Math.pow(10,9);
-    console.log(frequency + " GHz");
+
     let wavelength = (3 * Math.pow(10,8) / frequency);
-    console.log(wavelength);
+
     let gain = 0.7 * (22/7) * (22/7) * Math.pow(diameter / wavelength, 2);
-    console.log(gain);
+
     let gainInGHz = Math.floor(10 * Math.log10(gain));
-    console.log(gainInGHz + " dB");
+    
     let result =
     {
         "wavelength":wavelength,
