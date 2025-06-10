7264e18184839a3d04b0394d1352fd6febdf4290
vinay kumar <vk1230460@gmail.com>
2025-06-10 12:32:01 +0530

    first commit

--- controller/loss.controller.js
+++ controller/loss.controller.js
@@ -5,3 +5,16 @@ export let symbolRate = async (req,res)=>
     let formattedSymbolRate = symbolRate.toFixed(7);
     res.send({"symbolrate":formattedSymbolRate});
 }
+
+export let fspl =async(req,res)=>
+{
+    const SPEED_OF_LIGHT = 3 * Math.pow(10, 8);
+    let {distance,frequency,gtx,grx}=req.body;
+    frequency = 1* Math.pow(10, 9);
+    let fspl = 20 * Math.log10(distance) + 20 * Math.log10(frequency) + 20 * Math.log10((4 * Math.PI) / SPEED_OF_LIGHT) - gtx - grx;
+    let result =
+    {
+        "fspl":fspl.toFixed(2)
+    }
+    res.send(result);
+}
--- router/loss.router.js
+++ router/loss.router.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { symbolRate } from "../controller/loss.controller.js";
+import { fspl, symbolRate } from "../controller/loss.controller.js";
 
 let lossRouter = express.Router();
 lossRouter.post("/symbolrate", symbolRate);
+lossRouter.post("/fspl",fspl);
 export { lossRouter };
