0ea3fc91831f0bf3adc1932325776971a3171066
vinay kumar <vk1230460@gmail.com>
2025-07-24 13:33:46 +0530

    24-07-2025

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import 'dotenv/config'
 import cors from 'cors';
 import { dbConnect } from './config/db.js';
 import { satelliteRouter } from './router/satellite.router.js';
-import bodyParser from 'body-parser';
 import { angleRouter } from './router/angle.router.js';
 import { gainRouter } from './router/gain.router.js';
 import { lossRouter } from './router/loss.router.js';
